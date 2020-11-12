0d46e1602dbc168823a12962fe5279b25fe1250c
Dmitry K <dmitry@kuprianov.su>
2020-11-12 17:33:36 +0300

    Скопировал в таймлайн наброски истории

--- assets/js/mt-timeline.js
+++ assets/js/mt-timeline.js
@@ -1,31 +1,44 @@
 //Json Object
 var data = [
 	{
-		time: '1991-09-01',
-		header: 'Sample of header',
+		time: '1930-10-03',
+		header: 'Первое упоминание',
 		body: [{
 			tag: 'h1',
-			content: "Lorem ipsum"
+			content: 'Основываясь строго на документальных свидетельствах архива ИТМО,'
+		},
+		{
+			tag: 'p',
+			content: 'первое упоминание о прародительнице кафедры мехатроники содержится в приказе № 18 от 3.10.1930 по Учебному комбинату точной механики и оптики: “доцент Замыцкий Н.Н. назначен с 1.10.1930 заместителем заведующего кафедрой детали машин Института точной механики и оптики'
+		}],
+		footer: '<a href=\"https://museum.itmo.ru/page/36/\" target=\"_blank\">Источник - Сайт Музея ИТМО</a>'
+	},
+	{
+		time: '1989',
+		header: 'Тимофеев Б.П.',
+		body: [{
+			tag: 'h1',
+			content: 'ТМиДП'
+		},
+		{
+			tag: 'p',
+			content: 'В 1989 году Тимофеев Б.П. приступил к заведыванию общеинженерной кафедрой теории механизмов и деталей приборов, вскоре преобразованную в выпускающую кафедру мехатроники. Однако, сделав этот шаг первыми на территории бывшего СССР в то время, когда слова «Мехатроника» еще не было в квалификационном справочнике, мы были обречены на долгую и упорную борьбу. Лишь в 1994 году специальность 071800 «Мехатроника» появилась в официальном списке специальностей. Первый ГОС специальности был во многом основан на нашем учебном плане. С 2000 года университет имеет лицензию на образовательную деятельность по специальности “Мехатроника”.'
+		}],
+		footer: '<a href=\"https://museum.itmo.ru/page/36/\" target=\"_blank\">Источник - Сайт Музея ИТМО</a>'
+	},
+	{
+		time: '2012-10-12',
+		body: [{
+			tag: 'h1',
+			content: "Первое собрание инициативной группы"
 		},
 		{
 			tag: 'p',
 			content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa, suscipit eu elit urna in urna, gravida wisi aenean eros massa, cursus quisque leo quisque dui.'
 		}],
-		footer: 'Sample of footer. See <a href=\"https://github.com/Albejr/jquery-albe-timeline\" target=\"_blank\">more details</a>'
 	},
 	{
-		time: '2015-04-15',
-		body: [{
-			tag: 'h1',
-			content: "Basic content"
-		},
-		{
-			tag: 'p',
-			content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa, suscipit eu elit urna in urna, gravida wisi aenean eros massa, cursus quisque leo quisque dui.'
-		}],
-	},
-	{
-		time: '2016-01-20',
+		time: '2018-10-15',
 		body: [{
 			tag: 'img',
 			attr: {
@@ -36,7 +49,7 @@ var data = [
 		},
 		{
 			tag: 'h2',
-			content: 'Sample with image'
+			content: 'Первое собрание независимой лаборатории'
 		},
 		{
 			tag: 'p',
